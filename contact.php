44ec43eceb60001f5059ffad19641d6401773e39
Joe Mande <joemande243@gmail.com>
2022-07-24 12:51:40 +0200

    !

--- contact.php
+++ contact.php
@@ -1,17 +1,17 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-    $emailTo = "joemande243@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name."\n\n".$message;
+$emailTo = "joemande243@gmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail from ".$name."\n\n".$message;
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: joemande.tech?mailsend");
-}
+mail($mailTo, $subject, $txt, $headers)
+or die("Error!")
+
+echo {}
 
 ?>
